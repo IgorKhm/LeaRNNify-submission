faa16778a4034cb257b6a94ef8b4442f86bbb554
IgorKhm <igor.khme@gmail.com>
2020-09-16 10:49:06 +0200

    ik

--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
-from benchmarking import rand_pregenerated_benchmarks
+from benchmarking import rand_pregenerated_benchmarks, generate_rand_spec_and_check_them
 
 if __name__ == '__main__':
+    generate_rand_spec_and_check_them()
     rand_pregenerated_benchmarks(timeout=600, check_flows=True)
