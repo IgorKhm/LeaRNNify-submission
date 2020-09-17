4300b56576e538daec4b0ad70fb53accfeefcd71
IgorKhm <igor.khme@gmail.com>
2020-09-17 11:30:49 +0200

    adding files for each of the benchmarks - ik

---
+++ source/generate_rand_spec_and_check.py
@@ -0,0 +1,5 @@
+from benchmarking import generate_rand_spec_and_check_them
+
+if __name__ == '__main__':
+    generate_rand_spec_and_check_them(check_flows=True, timeout=600)
+
---
+++ source/rand_pregenerated_benchmarks.py
@@ -0,0 +1,5 @@
+from benchmarking import rand_pregenerated_benchmarks
+
+if __name__ == '__main__':
+    rand_pregenerated_benchmarks(timeout=600, check_flows=True)
+
